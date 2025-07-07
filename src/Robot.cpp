9a34e10ce92809382c181d8a4f6f2b030880ef73
Xiaozong-ZHAO <ucabhbn@ucl.ac.uk>
2025-07-07 20:02:01 +0100

    using right speed

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -354,7 +354,7 @@ void Robot::syncLogicalToPhysics(){
     
     physicsBody_->SetTransform(b2Vec2(position_(0), position_(1)), 0.0f);
     Eigen::VectorXd increment = ((*this)[1]->mu_ - (*this)[0]->mu_) * globals.TIMESTEP / globals.T0;
-    b2Vec2 desiredVel(increment(0) / globals.TIMESTEP, increment(1) / globals.TIMESTEP);
+    b2Vec2 desiredVel(increment(0), increment(1));
     physicsBody_->SetLinearVelocity(desiredVel);
 }
 
